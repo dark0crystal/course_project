d93d207b03b624ceffffdd7de3fdd516dc24eb8f
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-16 08:31:40 +0400

    add new content

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,13 @@ import 'package:flutter/material.dart';
 // because if we tell fultter that sth is a constant , it knows then the value will not change
 void main() {
   runApp(MaterialApp(
-    home: Text("Hello World , Hello Ninjas!!"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World ,We Are Commong Soon!"),
+        backgroundColor: Colors.indigo[300],
+        centerTitle: true,
+      ),
+      body: const Text("This is our new app ")
+    )
   ));
 }

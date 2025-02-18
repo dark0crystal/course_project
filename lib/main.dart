50c35ad2995cf2e6c9a047bfabee894dbee53d9e
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-18 08:43:45 +0400

    adding stateless widget stl

--- lib/main.dart
+++ lib/main.dart
@@ -19,3 +19,13 @@ void main() {
     )
   ));
 }
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

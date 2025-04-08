d799f81a81319c81f195d4ec0a064bc434ee0841
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-08 13:04:22 +0400

    fix

--- lib/widgets/image_gallery.dart
+++ lib/widgets/image_gallery.dart
@@ -6,40 +6,42 @@ class ImageGallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<String> images = [
-      'assets/images/oman1.jpeg',
-      'assets/images/oman2.webp',
-      'assets/images/oman3.jpeg',
-      'assets/images/oman4.webp',
+      'assets/oman1.jpeg',
+      'assets/oman2.webp',
+      'assets/oman3.jpeg',
+      'assets/oman4.webp',
     ];
 
     return Container(
-      height: 80,
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      height: 150,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal, // Enable horizontal scrolling
+        scrollDirection: Axis.horizontal,
         child: Row(
-          children: images.map((image) => Row(
-            children: [
-              // Image container
-              Container(
-                width: 80,
-                height: 80,
-                margin: const EdgeInsets.only(right: 8),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  image: DecorationImage(
-                    image: AssetImage(image),
-                    fit: BoxFit.cover,
+          children: images.map((image) => Container(
+            width: 120,
+            height: 120,
+            margin: const EdgeInsets.only(right: 12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  child: Container(
+                    width: 2, // Width of the grey line
+                    color: Colors.grey[400], // Grey color for the separator
                   ),
                 ),
-              ),
-              // Gray separator
-              Container(
-                width: 1,
-                height: 80,
-                color: Colors.grey, // Gray color separator
-              ),
-            ],
+              ],
+            ),
           )).toList(),
         ),
       ),

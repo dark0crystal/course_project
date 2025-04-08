112846ab1319422b7bdf7ae83d51de5704e4d449
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-08 18:47:49 +0400

    add new scrollable cards

---
+++ lib/widgets/TextCardScroller.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class TextCardScroller extends StatelessWidget {
+  const TextCardScroller({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> texts = [
+      'Card One',
+      'Card Two',
+      'Card Three',
+      'Card Four',
+      'Card Five',
+    ];
+
+    return SizedBox(
+      height: 80,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        itemCount: texts.length,
+        separatorBuilder: (context, index) => const SizedBox(width: 12),
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            alignment: Alignment.centerLeft,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  blurRadius: 5,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 4,
+                  height: 40,
+                  color: Colors.grey.shade400,
+                  margin: const EdgeInsets.only(right: 12),
+                ),
+                Text(
+                  texts[index],
+                  style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

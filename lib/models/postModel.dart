45c4a4260db4798700d495d3b1b5dcf75bfd17b2
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-20 07:19:19 +0400

    add post model

---
+++ lib/models/postModel.dart
@@ -0,0 +1,15 @@
+// Place_name		string
+// Location 		string  	
+// Description             string 
+// Rating		double
+// Governorate		string
+// Place_type		string
+
+class Postmodel {
+   String? Place_name;
+   String? Location;
+   String? Description;
+   int? Rating;
+   String? Governorate;
+   String? Place_type;
+}

a8c0a80e836155a582ad7fc2cded21a9a49e6bce
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-24 23:05:26 +0400

    add a scrollable posts

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -1,15 +1,65 @@
+import 'package:course_project/pages/place_details.dart';
 import 'package:flutter/material.dart';
+import 'package:course_project/models/postModel.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class PlaceListScreen extends StatelessWidget {
+  final List<Postmodel> places;
 
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
+  const PlaceListScreen({Key? key, required this.places}) : super(key: key);
 
-class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Places')),
+      body: ListView.builder(
+        itemCount: places.length,
+        itemBuilder: (context, index) {
+          final place = places[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PlaceDetailScreen(place: place),
+                ),
+              );
+            },
+            child: Card(
+              margin: EdgeInsets.all(10),
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                children: [
+                  Image.asset(
+                    'assets/images/placeholder.jpg', // Use actual image paths if available
+                    height: 180,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          place.placeName ?? 'Unnamed Place',
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 4),
+                        Text(place.governorate ?? '', style: TextStyle(color: Colors.grey)),
+                        SizedBox(height: 8),
+                        Text(place.description ?? '', maxLines: 2, overflow: TextOverflow.ellipsis),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }

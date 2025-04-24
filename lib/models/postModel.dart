c939e36a956df55d1e2370cabd6b29ff4694a2d9
ALMardasDEV <albusaidi9094@gmail.com>
2025-04-24 20:58:24 +0400

    add constructor for the post modal

--- lib/models/postModel.dart
+++ lib/models/postModel.dart
@@ -9,13 +9,15 @@
 // Approval		boolean
 
 class Postmodel {
-  String? Id;
-  String? User_id;
-  bool? Approval;		
-  String? Place_name;
-  String? Location;
-  String? Description;
-  int? Rating;
-  String? Governorate;
-  String? Place_type;
+  String? id;
+  String? userId;
+  bool? approval;		
+  String? placeName;
+  String? location;
+  String? description;
+  int? rating;
+  String? governorate;
+  String? placeType;
+
+  Postmodel({required this.id , required this.userId, required this.approval ,required this.description ,required this.governorate,required this.location,required this.placeName,required this.placeType,required this.rating});
 }

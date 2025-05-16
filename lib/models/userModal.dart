3abb2768257a353f676f5897b6282200564f7826
ALMardasDEV <albusaidi9094@gmail.com>
2025-05-16 20:54:25 +0400

    add use model

--- lib/models/userModal.dart
+++ lib/models/userModal.dart
@@ -1,19 +1,13 @@
-enum Role { user, admin }
-
 class User {
   final String id;
   final String userName;
   final String password;
   final String email;
-  final Role role;
-  final String profileImage;
 
   User({
     required this.id,
     required this.userName,
     required this.password,
     required this.email,
-    required this.role,
-    required this.profileImage,
   });
 }
